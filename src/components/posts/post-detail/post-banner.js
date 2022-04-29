608b68b8c4a6d3ccb31a24e641182506f2f87c01
Riley1101 <www.riley.s.eileen@gmail.com>
2022-04-29 21:58:07 +0630

    first commit

---
+++ src/components/posts/post-detail/post-banner.js
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+function PostBanner(props) {
+    const { title, image, date } = props;
+
+
+    return (
+        <div className='post-banner relative'>
+            <div className='image relative w-full lg:h-[980px] h-[550px] before:absolute before:bg-black before:opacity-40 before:w-full before:h-full before:z-[1]'>
+                <Image
+                    src={image}
+                    alt={title}
+                    layout='fill'
+                    objectFit='contain'
+                    priority
+                />
+            </div>
+            <div className='container'>
+                <div className='content absolute top-1/2 transform -translate-y-1/2 z-[1]'>
+                    <div className='inner-content relative z-[1] '>
+                        <h2 className='lg:text-[70px] lm:text-[40px] text-[20px] lg:leading-[80px] text-white max-w-[770px] mb-8'>
+                            {title}
+                        </h2>
+                        <Link href='/about'>
+                            <a className="boxed-btn text-[18px] leading-[30px] my-4">
+                                ORDER NOW
+                            </a>
+                        </Link>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PostBanner;
